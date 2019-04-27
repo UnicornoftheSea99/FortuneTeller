68c04a4b1a0ce9fd093590ffd42f07960529fe49
student admin <stuadmin@SMSWIN39-10843.ad.smith.edu>
2019-04-27 18:23:53 -0400

    small changes with code

---
+++ utils.js
@@ -0,0 +1,18 @@
+
+// helping function to use so we can easily send the server our status, 
+// our response and the data we have in application/json format
+exports.sendJSONObj = function(res, status, data) {
+    console.trace();
+    res.writeHead(status, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(data));
+    res.end();
+}
+
+// just added this function too because i notied we use it in 
+// app.js but it was never created 
+exports.sendError = function(res, data) {
+    console.trace();
+    res.writeHead(500, {'Content-Type': 'application/json'});
+    res.write(data);
+    res.end();
+}
