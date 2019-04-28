d426572eecf14a10c9002be74e7e4ea7492ff06f
UnicornoftheSea99 <erhyu@smith.edu>
2019-04-27 21:10:01 -0400

    moved css stuff, changed html file a lot, and figure out how get client input/show Fortune

--- public_html/fortune.js
+++ public_html/fortune.js
@@ -4,9 +4,45 @@
 // init();
 // loadQuestion();
 
-document.getElementById("submit").addEventListener("click", attempts)
+//this is what happens after client clicks submit button
+function check() {
 
-// attwmpts();
+    var1=document.quiz.question1.value; //this is the answer client pick to question 1
+
+    //makes whatever is in the aftersubmit div in html show up once this function called
+    document.getElementById("aftersubmit").style.visibility = "visible";
+
+    //23:50 in video
+    //HOW TO GET FORTUNES TO SHOW UP AT THE END
+    //fortunes is the array of fortunes, found in fortuneTeller.js
+    //range is the index
+    document.getElementById("message").innerHTML=fortunes[range];
+
+    //how to get images to show up with each fortune
+    //pictures is an array of picture names, which are in a file in public html
+    //range is index
+    document.getElementById("picture").src=pictures[range];
+}
+
+
+//OTHER STUFF
+//document.getElementById("submit").addEventListener("click", attempts)
+// attempts();
+
+//MIGHT USE LATER
+// function addMessage(msg) {
+//     console.log("showing the message");
+//     console.log(msg); //show message in console
+//     var para = document.createElement("p"); //creates a new paragraph element 
+//     para.innerHTML = "user: "+msg.sender;  //in new paragraph elem put in name of client sending message            
+//     var element = document.getElementById("mainDiv"); //refer to element in html file with id "mainDiv"
+//     element.appendChild(para); //add created paragraph to the existing html element
+
+//     var para = document.createElement("p"); //creates new paragraph element
+//     para.innerHTML = "Message: "+msg.message; //in new paragraph element put in message from other client             
+//     var element = document.getElementById("mainDiv"); //refer to element in html file with id "mainDiv"
+//     element.appendChild(para);//add created paragraph to the existing html element
+// }
 
 function attempts() {
     console.log("we're here because we're here because we're here")
@@ -27,9 +63,6 @@ function attempts() {
     xmlhttp.send();
 }
 
-// function startPage(){
-
-// }
 
 // function init(){
 //     var xmlhttp = new XMLHttpRequest();
