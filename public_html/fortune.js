ef62d813fe83126e6049ac63f0a5904242c410a3
student admin <stuadmin@SMSWIN39-10843.ad.smith.edu>
2019-04-27 23:29:56 -0400

    questions show up in different fields now!

--- public_html/fortune.js
+++ public_html/fortune.js
@@ -6,7 +6,7 @@
 
 //this is what happens after client clicks submit button
 function check() {
-    var1=document.quiz.question1.value; //this is the answer client pick to question 1
+    var1 = document.quiz.question1.value; //this is the answer client pick to question 1
 
     //makes whatever is in the aftersubmit div in html show up once this function called
     document.getElementById("aftersubmit").style.visibility = "visible";
@@ -15,12 +15,12 @@ function check() {
     //HOW TO GET FORTUNES TO SHOW UP AT THE END
     //fortunes is the array of fortunes, found in fortuneTeller.js
     //range is the index
-    document.getElementById("message").innerHTML=fortunes[range];
+    document.getElementById("message").innerHTML = fortunes[range];
 
     //how to get images to show up with each fortune
     //pictures is an array of picture names, which are in a file in public html
     //range is index
-    document.getElementById("picture").src=pictures[range];
+    document.getElementById("picture").src = pictures[range];
 }
 
 init();
@@ -40,31 +40,40 @@ function loadQs() {
     if (this.status == 200) {
         // just a string now because i have one question
         response = JSON.parse(this.responseText);
-        response.forEach(addQ);
+        // response.forEach(addQ);
+
+        qs = []
+        response.forEach(function(x){qs.push(x.q)});
+        
+        qs.forEach(addQ);
     }
 
     else alert("Error loading question");
 }
 
-function addQ(i) {
-    // questions 
+function addQ(q) {
+    console.log(q)
+    // response.forEach
+    var field = document.createElement("fieldset");
+    var element = document.getElementById("form");
+    element.appendChild(field);
+
     var h3 = document.createElement("legend");
-    console.log(i.q)
-    h3.innerHTML = i.q;
-    var element = document.getElementById("div1");
-    element.appendChild(h3); 
+    h3.innerHTML = JSON.stringify(q);
+    //var element = document.getElementById("field");
+    field.appendChild(h3);
 
     var h3 = document.createElement("input");
     h3.type = "radio";
     h3.id = "h3"
-    var element = document.getElementById("div1");
-    element.appendChild(h3);
+    //var element = document.getElementById("field");
+    field.appendChild(h3);
 
     var label = document.createElement("label");
     label.for = "h3";
     label.innerHTML = "chaou";
-    var element = document.getElementById("div1");
-    element.appendChild(label);
+    //var element = document.getElementById("field");
+    field.appendChild(label);
 
     // var h3 = document.createElement("h3");
     // one = i.option.forEach(function(x){
