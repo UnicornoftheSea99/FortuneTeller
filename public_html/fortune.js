16765966b505d5384976d3c0f77343b10cd93f31
student admin <stuadmin@SMSWIN39-10843.ad.smith.edu>
2019-04-30 22:34:05 -0400

    figured out values of answers for questions:

--- public_html/fortune.js
+++ public_html/fortune.js
@@ -9,46 +9,46 @@
 //https://tenor.com/view/fit-personalpenguintrainer-penguin-trainer-healthylifestyle-gif-4817050
 
 //this is what happens after client clicks submit button
-function check() {
-    //Object of fortunes and their values
+// function check() {
+//Object of fortunes and their values
 
-    // var q1 = document.quiz.question1.value; //this is the answer client pick to question 1
-    //var Q1=parseInt(q1);
-    // var q2 = document.quiz.question2.value;
-    // var Q2=parseInt(q2);
-    // var q3 = document.quiz.question3.value;
-    // var Q3=parseInt(q3);
-    // var q4 = document.quiz.question4.value;
-    // var Q4=parseInt(q4);
-    // var q5 = document.quiz.question5.value;
-    // var Q5=parseInt(q5);
-    // var q6 = document.quiz.question6.value;
-    // var Q6=parseInt(q6);
+// var q1 = document.quiz.question1.value; //this is the answer client pick to question 1
+//var Q1=parseInt(q1);
+// var q2 = document.quiz.question2.value;
+// var Q2=parseInt(q2);
+// var q3 = document.quiz.question3.value;
+// var Q3=parseInt(q3);
+// var q4 = document.quiz.question4.value;
+// var Q4=parseInt(q4);
+// var q5 = document.quiz.question5.value;
+// var Q5=parseInt(q5);
+// var q6 = document.quiz.question6.value;
+// var Q6=parseInt(q6);
 
-    //total value from the responses of each answer
-    // var totalFortuneValue = (Q1 + Q2 + Q3 + Q4 + Q5 + Q6);
+//total value from the responses of each answer
+// var totalFortuneValue = (Q1 + Q2 + Q3 + Q4 + Q5 + Q6);
 
-    //add if statements here to set range, which is index of fortunes and pictures
-    //example:
-    //var range;
-    //if (totalFortuneValue>=10){
-    //range=1;
-    //}
+//add if statements here to set range, which is index of fortunes and pictures
+//example:
+//var range;
+//if (totalFortuneValue>=10){
+//range=1;
+//}
 
-    //makes whatever is in the aftersubmit div in html show up once this function called
-    document.getElementById("aftersubmit").style.visibility = "visible";
+//makes whatever is in the aftersubmit div in html show up once this function called
+// document.getElementById("aftersubmit").style.visibility = "visible";
 
-    //23:50 in video
-    //HOW TO GET FORTUNES TO SHOW UP AT THE END
-    //fortunes is the array of fortunes, found in fortuneTeller.js
-    //range (right now 0) is the index
-    document.getElementById("message").innerHTML = fortunes[0].fortune;
+//23:50 in video
+//HOW TO GET FORTUNES TO SHOW UP AT THE END
+//fortunes is the array of fortunes, found in fortuneTeller.js
+//range (right now 0) is the index
+// document.getElementById("message").innerHTML = fortunes[0].fortune;
 
-    //how to get images to show up with each fortune
-    //pictures is an array of picture names, which are in a file in public html
-    //range is index
-    document.getElementById("picture").src = pictures[0];
-}
+//how to get images to show up with each fortune
+//pictures is an array of picture names, which are in a file in public html
+//range is index
+// document.getElementById("picture").src = pictures[0];
+// }
 
 init();
 
@@ -92,7 +92,7 @@ function addQ(obj, counter) {
         var ops = document.createElement("input");
         ops.type = "radio";
         ops.id = x.value;
-        ops.name = "options" + counter;
+        ops.name = "ops" + counter;
         // ops.value = "som" + counter2;
         field.appendChild(ops);
 
@@ -108,12 +108,23 @@ function addQ(obj, counter) {
     })
 }
 
-function getValueOfAnswers(){
-    document.getElementById("s").checked;
+function getValueOfAnswers() {
+    // query selector loops through all elements that are inputs, with the
+    // corresponding name, whether it's selected and saves its id
+    val1 = document.querySelector('input[name="ops1"]:checked').id;
+    val2 = document.querySelector('input[name="ops2"]:checked').id;
+    val3 = document.querySelector('input[name="ops3"]:checked').id;
+    val4 = document.querySelector('input[name="ops4"]:checked').id;
+    val5 = document.querySelector('input[name="ops5"]:checked').id;
+    val6 = document.querySelector('input[name="ops6"]:checked').id;
+
+    return totalVal = val1 + val2 + val3 + val4 + val5 + val6;
 }
+
 
 //not needed? can just do within fortune.js
 function getFortune() {
+    console.log("we're in here too")
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onload = loadFortune;
     xmlhttp.onerror = function () { alert("Error loading start page") };
@@ -123,13 +134,13 @@ function getFortune() {
     xmlhttp.send();
 }
 
-// //not needed?
-// function loadFortune() {
-//     if (this.status == 200) {
-//         // just a string now because i have one question
-//         response = this.responseText;
-//     }
-// }
+//not needed?
+function loadFortune() {
+    if (this.status == 200) {
+        // just a string now because i have one question
+        response = this.responseText;
+    }
+}
 
 function queryObjectToString(query) {
     // console.log(query);
@@ -150,3 +161,5 @@ function handleSpaces(str) {
     }
     return newStr;
 }
+
+document.getElementById("HATE").onclick = getFortune;
